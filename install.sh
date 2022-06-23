5eb8f5ec935e209b41b44a5d19a836bab2616c5a
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2022-06-23 16:56:03 +0700

    edit install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 echo "========================"
 echo "git clone file config nvim"
-git clone https://github.com/bobbyrahmanda13/config-nvim-lua
-cd config-nvim-lua && mkdir ../.config && mv nvim ../.config/
+git clone https://github.com/bobbyrahmanda13/nvim-config/
+cd nvim-config && mkdir ../.config/nvim && mv * ../.config/
