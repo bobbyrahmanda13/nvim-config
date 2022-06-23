9db1c5d57cb31178e3a18cf9427978b14d57d859
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2022-06-23 21:56:06 +0700

    update plugins setting null-ls format on save

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -30,8 +30,25 @@ local formatting = null_ls.builtins.formatting
 local sources = {
  formatting.prettier
 }
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 null_ls.setup({
-sources = sources
+sources = sources,
+on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                    vim.lsp.buf.format({ bufnr = bufnr })
+                   -- vim.lsp.buf.formatting_sync()
+                end,
+            })
+        end
+    end,
+    
 })
 -- null-ls plugin end
 
