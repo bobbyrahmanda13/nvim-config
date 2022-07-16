dba4c1315c25d083767e9ecd970bb0b82ca300cb
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2022-07-16 20:17:11 +0700

    update config nvim plugin null-ls debug true

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -36,6 +36,7 @@ local sources = {
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
+debug = true,
 sources = sources,
 on_attach = function(client, bufnr)
         if client.supports_method("textDocument/formatting") then
